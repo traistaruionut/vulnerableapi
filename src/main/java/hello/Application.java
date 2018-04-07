1c77f6561aa63ce41c1b7c252515ff7cd97e8a29
Traistaru Ionut <traistaru.ionutt@gmail.com>
2018-04-07 07:25:45 -0700

    add springboot mvn plugin

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan({"config","controllers"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
