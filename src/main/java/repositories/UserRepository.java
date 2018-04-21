e54d00591132b5985fd65f58c4cd4453d42f5271
Traistaru Ionut <traistaru.ionutt@gmail.com>
2018-04-21 13:27:52 -0700

    created crud user repository

---
+++ src/main/java/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interfacegit  UserRepository extends CrudRepository<User,Long> {
+}
